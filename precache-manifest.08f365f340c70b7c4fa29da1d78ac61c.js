2ae11b04009c794ebb12dc759cbfafa2b70e6acb
nick lachance <nicholasplachance@gmail.com>
2019-12-13 19:05:07 -0600

    Updates

---
+++ precache-manifest.08f365f340c70b7c4fa29da1d78ac61c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a81d7c3b6a98c87a603a6db699388e2",
+    "url": "/monster_roledex_reactjs_practiceProjcet/index.html"
+  },
+  {
+    "revision": "859ce6ceb27fcc0fdb87",
+    "url": "/monster_roledex_reactjs_practiceProjcet/static/css/main.e6c87595.chunk.css"
+  },
+  {
+    "revision": "fd6ebc4e82ad40ffe41b",
+    "url": "/monster_roledex_reactjs_practiceProjcet/static/js/2.c31d5ea1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster_roledex_reactjs_practiceProjcet/static/js/2.c31d5ea1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "859ce6ceb27fcc0fdb87",
+    "url": "/monster_roledex_reactjs_practiceProjcet/static/js/main.c4972374.chunk.js"
+  },
+  {
+    "revision": "25e2c101293823a1a111",
+    "url": "/monster_roledex_reactjs_practiceProjcet/static/js/runtime-main.dfe6edf2.js"
+  }
+]);
